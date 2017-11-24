65cbb79849621f730468bcccd2e4cde429d1a0ff
yuansong <yuanxs@szzl.com>
2017-11-24 18:21:49 +0800

    新增 生成requirements.txt的批处理

---
+++ getRequireMents.bat
@@ -0,0 +1,3 @@
+call d:\Python\Python36_VirtualEnv\PyTest\Scripts\activate.bat
+d:\Python\Python36_VirtualEnv\PyTest\Scripts\pip freeze > D:\PyProj\PyTest\requirements.txt
+call d:\Python\Python36_VirtualEnv\PyTest\Scripts\deactivate.bat
