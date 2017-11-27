7a6d60414b61800ac52c341acb3bbaee07c9b4a1
yuansong <yuanxs@szzl.com>
2017-11-27 17:22:54 +0800

    t

--- HttpTest/HttpTest.py
+++ HttpTest/HttpTest.py
@@ -2,21 +2,31 @@ import urllib.request
 import urllib.parse
 
 
-def get_http_request(url = "http://www.baidu.com"):
+def get_http_request(url="http://www.baidu.com"):
     f = urllib.request.urlopen(url)
     return f.read().decode('utf-8')
     print(f.read().decode('utf-8'))
 
 
-def test():
+def create_data_line():
     no1 = get_http_request("http://123.57.226.114:9049/Api/Number/GetSno?workerId=1&&prefix=CR")
     no2 = get_http_request("http://123.57.226.114:9049/Api/Number/GetSno?workerId=1&&prefix=CR")
     no3 = get_http_request("http://123.57.226.114:9049/Api/Number/GetId?workerId=1")
-    # print(no1 + "," + no2 + "," + no3)
+    return no1 + "," + no2 + "," + no3
 
 
 if __name__ == "__main__":
-    i = 0
-    while i < 100:
-        test()
-        i = i + 1
+    data = []
+    for num in range(100):
+        print(num)
+        data.append(create_data_line())
+    # data.append("a")
+    # data.append("b")
+    # data.append("c")
+    # data.append("d")
+    with open('test.txt', 'w') as fw:
+        for letter in data:
+            fw.writelines(letter + '\n')
+
+
+
