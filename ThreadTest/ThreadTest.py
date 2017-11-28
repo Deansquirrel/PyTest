b9b5bbd2f0ff0be49bfe0a698db9d375e71388cd
yuansong <yuanxs@szzl.com>
2017-11-28 18:07:30 +0800

    Add Thread

--- ThreadTest/ThreadTest.py
+++ ThreadTest/ThreadTest.py
@@ -8,16 +8,26 @@ from time import ctime,sleep
 def music(func):
     for i in range(2):
         print("I was listening to music %s. %s" % (func, ctime()))
-        sleep(1)
+        sleep(2)
 
 
 def move(func):
     for i in range(2):
         print("I was at the %s! %s" % (func, ctime()))
-        sleep(5)
+        sleep(1)
 
 
 if __name__ == '__main__':
-    music('AAA')
-    move('BBB')
+    threads = []
+    t1 = threading.Thread(target=music, args=("AAA",))
+    threads.append(t1)
+    t2 = threading.Thread(target=move, args=("BBB",))
+    threads.append(t2)
+    for t in threads:
+        t.setDaemon(True)
+        t.start()
+        print(t)
+    print("123")
+    t.join()
+    print(t)
     print("all over %s" % ctime())
