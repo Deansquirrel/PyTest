1e3aca3cbebbf3dadaaffd8cd9bb9b749a4b075c
yuansong <yuanxs@szzl.com>
2017-11-29 10:23:37 +0800

    modify Thread

--- HttpTest/HttpTest.py
+++ HttpTest/HttpTest.py
@@ -2,19 +2,24 @@
 import threading
 import urllib.request
 import urllib.parse
-from time import ctime
+import time
 
 
 def get_http_request(url="http://www.baidu.com"):
-    f = urllib.request.urlopen(url)
+    try:
+        f = urllib.request.urlopen(url)
+    except:# Exception as e:
+        # print(e)
+        return "超时"
     return f.read().decode('utf-8')
     # print(f.read().decode('utf-8'))
 
 
 def create_data_line():
-    no1 = get_http_request("http://123.57.226.114:9049/Api/Number/GetSno?workerId=1&&prefix=CR")
-    no2 = get_http_request("http://123.57.226.114:9049/Api/Number/GetSno?workerId=1&&prefix=CR")
-    no3 = get_http_request("http://123.57.226.114:9049/Api/Number/GetId?workerId=1")
+    # 123.57.226.114:9049
+    no1 = get_http_request("http://106.14.57.245:10001/Api/Number/GetSno?workerId=1&&prefix=CR")
+    no2 = get_http_request("http://106.14.57.245:10001/Api/Number/GetSno?workerId=1&&prefix=CR")
+    no3 = get_http_request("http://106.14.57.245:10001/Api/Number/GetId?workerId=1")
     return no1 + "," + no2 + "," + no3
 
 
@@ -23,42 +28,18 @@ def get_result(index):
 
 
 if __name__ == "__main__":
-    print("%s" % (ctime()))
+    start = time.time()
     threads = []
-    for i in range(10):
-        print(i)
+    for i in range(1000):
         thread = threading.Thread(target=get_result, args=(str(i),))
         threads.append(thread)
-
     for t in threads:
         t.setDaemon(True)
         t.start()
-    t.join()
-    print("%s" % (ctime()))
-
-    # threads = []
-    # t1 = threading.Thread(target=music, args=("AAA",))
-    # threads.append(t1)
-    # t2 = threading.Thread(target=move, args=("BBB",))
-    # threads.append(t2)
-    # for t in threads:
-    #     t.setDaemon(True)
-    #     t.start()
-    #     print(t)
-    # print("123")
-    # t.join()
-    # print(t)
-    # print("all over %s" % ctime())
-    #
-    #
-    # data = []
-    # for num in range(100):
-    #     print(num)
-    #     data.append(create_data_line())
-    # # data.append("a")
-    # # data.append("b")
-    # # data.append("c")
-    # # data.append("d")
-    # with open('test.txt', 'w') as fw:
-    #     for letter in data:
-    #         fw.writelines(letter + '\n')
+        time.sleep(0.1)
+    for t in threads:
+        t.join()
+    end = time.time()
+    print("%s" % (start,))
+    print("%s" % (end,))
+    print(end - start)
